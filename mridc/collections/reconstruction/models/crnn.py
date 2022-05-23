0c5d37a525d29bfc406648933c6193aebe353a50
wdika <d.karkalousos@amsterdamumc.nl>
2022-05-23 21:46:02 +0200

    fixes documentation

--- mridc/collections/reconstruction/models/crnn.py
+++ mridc/collections/reconstruction/models/crnn.py
@@ -108,7 +108,7 @@ class CRNNet(BaseMRIReconstructionModel, ABC):
         """
         sensitivity_maps = self.sens_net(y, mask) if self.use_sens_net else sensitivity_maps
         pred = self.crnn(y, sensitivity_maps, mask)
-        yield [self.process_intermediate_eta(x, sensitivity_maps, target) for x in pred]
+        yield [self.process_intermediate_pred(x, sensitivity_maps, target) for x in pred]
 
     def process_intermediate_pred(self, pred, sensitivity_maps, target):
         """
